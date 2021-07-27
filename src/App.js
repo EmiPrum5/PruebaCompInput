a3fe1ea8a7f2ba0b9cc0f29d4779f96ec426551f
EmiPrum5 <emiliot00@hotmail.com>
2021-07-27 18:00:03 -0300

    modaf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import MyForm from './component/MyForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      datos : [
+        { datain: 'Email' , tinp: 'email', required: true},
+        { datain: 'Password' , tinp: 'password', required: true}
+      ],
+      datbutton : 'Ingresar'
+    }
+  }
+
+  recepcionDatos = (datos) => {
+    console.log('El email ingresado es: ' + datos.Email);
+    console.log('Contrasenia ' + datos.Password);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <MyForm datos = {this.state.datos} datbutton = {this.state.datbutton} recepcionDatos = {this.recepcionDatos}/>
+      </div>
+    );
+    }
 }
 
 export default App;
---
+++ src/component/MyForm.jsx
@@ -0,0 +1,63 @@
+import React, {Component, useEffect, useState} from 'react'
+
+const MyForm = (props) => {
+
+    const parsein = () => {
+        let arraux = [];
+        props.datos.map((dato) => {
+            arraux.push({[dato.datain] : ''}) 
+            
+            }
+        )
+        return arraux;
+    }
+
+    const [datos, setDatos] = useState(props.datos);
+
+    const [inputdata, setInputdata] = useState();
+
+    const datbutton = useState(props.datbutton);
+
+    const handleInputChange = ( event ) => {
+        console.log( event.target.name)
+        setInputdata({
+            ...inputdata,
+            [event.target.name] : event.target.value
+        })
+    }
+    
+
+    /*useEffect((event) => {
+        console.log( event.target.name);
+        this.setDatos({
+            [event.target.name] : event.target.value
+        })
+    })*/
+    
+
+    const enviarDatos = (event) => {
+        event.preventDefault();
+        props.recepcionDatos(inputdata);
+      }
+
+    return(             
+       <form className = "CompInput" name= "form" onSubmit = {enviarDatos}>           
+            {datos.map((dato, index) => 
+                <input 
+                    key = {index}
+                    text={dato.tinp} 
+                    onChange = { handleInputChange } 
+                    placeholder = {dato.datain} 
+                    name = {dato.datain} 
+                />
+                )                                           
+            }
+            <button>
+                {datbutton}
+            </button>
+       </form>       
+
+    )
+}
+
+export default MyForm;
